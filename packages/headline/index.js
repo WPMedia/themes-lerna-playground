aacbbea3bb5ac22411b4bae53bcb08e9087ae045
Jack Howard <jack.howard@washpost.com>
2021-07-20 13:20:46 -0500

    feat: Initial setup

---
+++ packages/headline/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Headline() {
+  return (
+    <h1>Headline</h1>
+  );
+}
+
+export default Headline;
---
+++ packages/sub-headline/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SubHeadline({ children }) {
+  return (
+    <h2>
+      {children}
+    </h2>
+  );
+}
+
+export default SubHeadline;
