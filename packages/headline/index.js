892285709eca70920e757a5c3482830f6ec76fe0
Jack Howard <jack.howard@washpost.com>
2022-01-14 11:13:46 -0600

    feat: New headline publish

--- packages/headline/index.js
+++ packages/headline/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Headline() {
   return (
-    <h1>Only headline update one block first after for arc-themes-release-1.19</h1>
+    <h1>Only headline update one block first after for arc-themes-release-1.20 publish</h1>
   );
 }
 
