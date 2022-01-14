2789c6cf4d5623f2c001d15282cdafd501d8d550
Jack Howard <jack.howard@washpost.com>
2022-01-14 11:19:45 -0600

    feat: Update description package only

--- packages/description/index.js
+++ packages/description/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Description() {
   return (
     <div>
-      <h1>Description change for arc-themes-release-1.18</h1>  
+      <h1>Description change for arc-themes-release-1.21 publish</h1>  
     </div>
   )
 }
