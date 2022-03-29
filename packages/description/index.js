e5d7221a1765c6665bd24cb6a6ca2813a5776cd4
Jack Howard <jackhowa@fastmail.com>
2022-03-29 11:03:30 -0500

    feat: Change to description

--- packages/description/index.js
+++ packages/description/index.js
@@ -3,7 +3,8 @@ import React from 'react';
 function Description() {
   return (
     <div>
-      <h1>Description: another change</h1>  
+      <h1>Description: anot
+      her change</h1>  
     </div>
   )
 }
